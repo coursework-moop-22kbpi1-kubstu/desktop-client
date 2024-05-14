1344e325d65259a99c7c14e6fef5fde05dcb5c74
DP1415 <Daniil-2005-zima@mail.ru>
2024-05-14 16:33:28 +0300

    start

---
+++ WindowsFormsApp/WindowsFormsApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Model model = new Model();
+            ViewModel viewModel = new ViewModel();
+            DesktopAppForm view = new DesktopAppForm();
+            model.viewModel = viewModel;
+            viewModel.view = view;
+            viewModel.model = model;
+            view.viewModel = viewModel;
+
+            Application.Run(view);
+        }
+    }
+}
